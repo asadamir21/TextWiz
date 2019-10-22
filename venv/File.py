2701c4a0587ddbd1b6d3a3ca34263f000c5ea34c
Asad <asadamir200@gmail.com>
2019-10-22 21:11:01 +0500

    Version 1.1

---
+++ venv/File.py
@@ -0,0 +1,20 @@
+
+class File():
+    def __init__(self):
+        super().__init__()
+
+    def setFileName(self, name):
+        self.FileName = name
+
+    def setFileLocation(self, path):
+        self.FileLocation = path
+
+    def setCreatedDate(self, date):
+        self.CreatedDate = date
+
+    def setModifiedDate(self, date):
+        self.ModifiedDate = date
+
+    def setModifiedBy(self, name):
+        self.ModifiedBy = name
+

01f7b47b9eb7237ef10031ef6f02caa10ce2c96c
Farmal <mfarmalkhan@khi.iba.edu.pk>
2019-11-10 22:18:39 +0500

    version 1.12

--- venv/sentiment_analysis.py
+++ venv/sentiment_analysis.py
@@ -9,6 +9,9 @@ import string
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.model_selection import train_test_split
 
 
 def text_preprocessing(document_text):
@@ -40,9 +43,21 @@ def text_preprocessing(document_text):
     for word in result:
         final_text.append(lem.lemmatize(word))
 
-    print(final_text)
-
+    # print(final_text)
+    return final_text
 
 filename = "C:/Users/farma/Desktop/internship docs/submissions/IBA INTERNSHIP REPORT.docx"
 document_text = read_file(filename,get_extension(filename))
-text_preprocessing(document_text)
+documents = text_preprocessing(document_text)
+
+# vectorizer = CountVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
+# X = vectorizer.fit_transform(documents).toarray()
+#
+# tfidfconverter = TfidfTransformer()
+# X = tfidfconverter.fit_transform(X).toarray()
+#
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+#
+# classifier = RandomForestClassifier(n_estimators=1000, random_state=0)
+# classifier.fit(X_train, y_train)
+# y_pred = classifier.predict(X_test)
